821bb0688560269f9734cef2e36544bc099b973b
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2025-07-18 21:48:54 +0800

    additionals

---
+++ assets/additionals/cordova/inAppBrowser.js
@@ -0,0 +1,17 @@
+console.log("Additionals - Cordova: In App Browser ")
+// Open all links in InAppBrowser //
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    window.open = cordova.InAppBrowser.open;
+}
+
+document.addEventListener('click', function (event) {
+    console.log('click');
+    console.log(event.target);
+  // Check if the clicked element is an anchor or inside one
+  const anchor = event.target.closest('a');
+  if (event.target.tagName == "A" && anchor && anchor.href) {
+    event.preventDefault(); // Cancel the default navigation
+    cordova.InAppBrowser.open(anchor.href, '_blank'); // Open the link in a new tab/window
+  }
+});
